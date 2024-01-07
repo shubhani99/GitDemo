b7383c10753e1b0b9bf90bc60be44d3f224ae32c
shubhani99 <shubhanimehrotra@gmail.com>
2024-01-07 19:42:09 +0530

    First Commit Msg

---
+++ src/test/java/qaclickacademy/AppTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Hola mundo!!");
+    }
+}
