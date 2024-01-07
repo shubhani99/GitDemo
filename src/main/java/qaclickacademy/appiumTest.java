6214be31ee7e5bb096143d5a87c6527d1bfbe824
shubhani99 <shubhanimehrotra@gmail.com>
2024-01-07 19:57:35 +0530

    Asian architect development

--- src/main/java/qaclickacademy/appiumTest.java
+++ src/main/java/qaclickacademy/appiumTest.java
@@ -1,15 +1,15 @@
 package qaclickacademy;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class apium 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "dfsdsdAAAs" );
+
         System.out.println( "Hello World!2" );
         System.out.println( "Hello World!222" );
     }
